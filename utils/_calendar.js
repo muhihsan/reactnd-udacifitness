9b10678e5e1735f4136505523290592fd0fc6414
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-19 23:26:19 +1100

    Add AddEntry component to render bike icon

--- utils/_calendar.js
+++ utils/_calendar.js
@@ -5,11 +5,11 @@ import { getMetricMetaInfo, timeToString } from './helpers'
 
 export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar'
 
-function getRandomNumber (max) {
+function getRandomNumber(max) {
   return Math.floor(Math.random() * max) + 0
 }
 
-function setDummyData () {
+function setDummyData() {
   const { run, bike, swim, sleep, eat } = getMetricMetaInfo()
 
   let dummyData = {}
@@ -20,12 +20,12 @@ function setDummyData () {
     const strTime = timeToString(time)
     dummyData[strTime] = getRandomNumber(3) % 2 === 0
       ? {
-          run: getRandomNumber(run.max),
-          bike: getRandomNumber(bike.max),
-          swim: getRandomNumber(swim.max),
-          sleep: getRandomNumber(sleep.max),
-          eat: getRandomNumber(eat.max),
-        }
+        run: getRandomNumber(run.max),
+        bike: getRandomNumber(bike.max),
+        swim: getRandomNumber(swim.max),
+        sleep: getRandomNumber(sleep.max),
+        eat: getRandomNumber(eat.max),
+      }
       : null
   }
 
@@ -34,7 +34,7 @@ function setDummyData () {
   return dummyData
 }
 
-function setMissingDates (dates) {
+function setMissingDates(dates) {
   const length = Object.keys(dates).length
   const timestamp = Date.now()
 
@@ -50,7 +50,7 @@ function setMissingDates (dates) {
   return dates
 }
 
-export function formatCalendarResults (results) {
+export function formatCalendarResults(results) {
   return results === null
     ? setDummyData()
     : setMissingDates(JSON.parse(results))
