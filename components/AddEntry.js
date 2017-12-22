56b4ca1efcca4e8f1b9f16d6b8603e5aebe481d0
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-23 07:22:51 +1100

    Add button

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { getMetricMetaInfo } from '../utils/helpers';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciStepper from './UdaciSteppers';
 import DateHeader from './DateHeader';
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -42,6 +51,19 @@ export default class AddEntry extends Component {
     }))
   }
 
+  submit = () => {
+    const key = timeToString();
+    const entry = this.state;
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0
+    }))
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo();
     return (
@@ -69,6 +91,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     );
   }
