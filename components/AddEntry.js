18c0b043b3131641f7d6c47a25453b10d306f0b1
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-23 07:41:14 +1100

    Add if logged UI

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciStepper from './UdaciSteppers';
 import DateHeader from './DateHeader';
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton';
 
 function SubmitBtn({ onPress }) {
   return (
@@ -61,11 +63,31 @@ export default class AddEntry extends Component {
       swim: 0,
       sleep: 0,
       eat: 0
-    }))
+    }));
+  }
+
+  reset = () => {
+    const key = timeToString();
   }
 
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy-outline'
+            size={100}
+          />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      );
+    }
+
     return (
       <View>
         <DateHeader date={(new Date()).toLocaleDateString()} />
