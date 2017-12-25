3271b106cb84e0660d03bcf32be516592fb2dcb1
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-26 10:15:54 +1100

    Go back to home page after adding and resetting entry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,7 @@ import { submitEntry, removeEntry } from '../utils/api';
 import { connect } from 'react-redux';
 import { addEntry } from '../actions';
 import { white, purple } from '../utils/colors';
+import { NavigationActions } from 'react-navigation';
 
 function SubmitBtn({ onPress }) {
   return (
@@ -74,6 +75,8 @@ class AddEntry extends Component {
       eat: 0
     }));
 
+    this.toHome();
+
     submitEntry({ key, entry });
   }
 
@@ -84,7 +87,15 @@ class AddEntry extends Component {
       [key]: getDailyReminderValue()
     }));
 
+    this.toHome();
+
     removeEntry(key);
+  }
+
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({
+      key: 'AddEntry'
+    }));
   }
 
   render() {
@@ -98,7 +109,7 @@ class AddEntry extends Component {
             size={100}
           />
           <Text>You already logged your information for today</Text>
-          <TextButton style={{padding: 10}} onPress={this.reset}>
+          <TextButton style={{ padding: 10 }} onPress={this.reset}>
             Reset
           </TextButton>
         </View>
